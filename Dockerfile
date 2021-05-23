27161c62a2a7f01e340cf56904c1e4b3648f70a0
Henny022p <info@henny022.de>
2021-05-23 12:05:35 +0200

    new pycc

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ RUN dkp-pacman -Syyu --noconfirm gba-dev && \
 ENV DEVKITARM=${DEVKITPRO}/devkitARM
 
 RUN apt-get update \
-    && DEBIAN_FRONTEND=noninteractive apt-get -y install apt-transport-https wget dirmngr build-essential git binutils-arm-none-eabi libsndfile1-dev libpng-dev python3 && curl -sL https://deb.nodesource.com/setup_12.x | bash - && apt-get -y install nodejs \
+    && DEBIAN_FRONTEND=noninteractive apt-get -y install apt-transport-https wget dirmngr build-essential git binutils-arm-none-eabi libsndfile1-dev libpng-dev python3 python3-pip && curl -sL https://deb.nodesource.com/setup_12.x | bash - && apt-get -y install nodejs \
     && rm -rf /var/lib/apt/lists/*
 COPY agbcc /agbcc
 RUN cd /agbcc && ./build.sh && ./install.sh /agbcc_build
@@ -17,5 +17,6 @@ RUN cd /ce && make prereqs webpack WEBPACK_ARGS="-p"
 RUN mkdir -p /repos && cd /repos && git clone https://github.com/zeldaret/tmc.git && cd tmc && make setup
 COPY update-repo.sh /scripts/update-repo.sh
 COPY frontends /frontends
+RUN pip3 install -r /frontends/requirements.txt
 EXPOSE 10240
 CMD cd /ce && ./node_modules/.bin/supervisor -w app.js,lib,etc/config -e 'js|node|properties|yaml' --exec /usr/bin/node  -- -r esm ./app.js
---
+++ frontends/pycc.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import subprocess
+import os
+import argparse
+from parser import parse, ASTGenerator, ASTDump
+
+parser = argparse.ArgumentParser(description='Simplified CC1 frontend')
+
+parser.add_argument('--qinclude', action='append', help='Include Paths for iquote', required=False, dest='qinclude')
+parser.add_argument('--binclude', action='append', help='Include Paths for Block Include', required=False,
+                    dest='binclude')
+parser.add_argument('--cc1', action='store', help='<Required> cc1 Path', required=False, dest='cc1')
+parser.add_argument('--version', action='store', help='Get Version String of cc1', required=False, dest='version')
+parser.add_argument('--preproc', action='store', help='preproc path', required=False, dest='preproc')
+parser.add_argument('--charmap', action='store', help='preproc charmap', required=False)
+parser.add_argument('-S', action='store_true', help='Ignore parameter as agbcc does not know it', required=False)
+parser.add_argument('-o', action='store', help='Output Assembly file', required=False, dest='destination')
+
+args, remainder = parser.parse_known_args()
+'''
+repopath = "/repos/tmc/"
+agbccpath = "/agbcc_build/tools/"
+agbcc = agbccpath+ "agbcc/bin/agbcc"
+preproc = repopath+ "tools/preproc/preproc"
+charmap = repopath+ "charmap.txt"
+'''
+if args.version:
+    git_proc = subprocess.run(['git', '--git-dir=' + args.version + '/.git', 'rev-parse', '--short', 'HEAD'],
+                              stdout=subprocess.PIPE)
+    print("pycc frontend for agbcc1 " + os.path.basename(args.version) + "@" + git_proc.stdout.decode('utf-8'))
+    exit(0)
+
+source = remainder[-1]
+cpp_args = ["cpp", "-nostdinc", "-undef"]
+
+# Add Block Includes and Quote Includes
+if args.qinclude:
+    for q in args.qinclude:
+        cpp_args += ["-iquote", q]
+
+if args.binclude:
+    for b in args.binclude:
+        cpp_args += ["-I", b]
+
+cpp_args += [source, "-o", source + ".i"]
+subprocess.call(cpp_args)
+if args.preproc and args.charmap:
+    pprocess = subprocess.Popen([args.preproc, source + '.i', args.charmap], stdout=subprocess.PIPE)
+    subprocess.call([args.cc1] + ['-o', args.destination + '.tmp'] + remainder[0:-1], stdin=pprocess.stdout)
+else:
+    with open(source + '.i', 'r') as a:
+        subprocess.call([args.cc1] + ['-o', args.destination + '.tmp'] + remainder[0:-1], stdin=a)
+
+tree, success = parse(args.destination + '.tmp')
+if not success:
+    raise ValueError('bad input file')
+ast = ASTGenerator().visit(tree)
+with open(args.destination, 'w') as f_dst:
+    ASTDump(f_dst).visit(ast)
+
+if os.path.exists(source + '.i'):
+    os.remove(source + '.i')
+'''
+source = sys.argv[-1]
+subprocess.call(["cpp", "-nostdinc", "-undef", "-iquote", repopath + "include" , "-I", agbccpath + "agbcc", "-I", agbccpath + "agbcc/include", source, "-o", source+".i"])
+pprocess = subprocess.Popen([preproc, source+".i", charmap], stdout=subprocess.PIPE)
+subprocess.call([agbcc] + sys.argv[1:-1], stdin=pprocess.stdout)
+
+if os.path.exists(source+".i"):
+        os.remove(source+".i")
+'''
