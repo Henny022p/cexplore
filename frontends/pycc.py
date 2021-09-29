ecb3350f6dfe85380216ea7742bcae4c53c859da
Henny022p <info@henny022.de>
2021-09-29 13:34:45 +0200

    cleaned pycc script

--- frontends/pycc.py
+++ frontends/pycc.py
@@ -1,76 +1,87 @@
 #!/usr/bin/env python3
 
-import subprocess
-import os
 import argparse
+import os
+import subprocess
+import sys
+from shutil import copyfile
+
 from parser import parse, generate_ast, apply_transformations, ASTDump
 
-parser = argparse.ArgumentParser(description='Simplified CC1 frontend')
 
-parser.add_argument('--qinclude', action='append', help='Include Paths for iquote', required=False, dest='qinclude')
-parser.add_argument('--binclude', action='append', help='Include Paths for Block Include', required=False,
-                    dest='binclude')
-parser.add_argument('--cc1', action='store', help='<Required> cc1 Path', required=False, dest='cc1')
-parser.add_argument('--version', action='store', help='Get Version String of cc1', required=False, dest='version')
-parser.add_argument('--preproc', action='store', help='preproc path', required=False, dest='preproc')
-parser.add_argument('--charmap', action='store', help='preproc charmap', required=False)
-parser.add_argument('-S', action='store_true', help='Ignore parameter as agbcc does not know it', required=False)
-parser.add_argument('-o', action='store', help='Output Assembly file', required=False, dest='destination')
+def parse_args(argv):
+    parser = argparse.ArgumentParser(description='Simplified CC1 frontend')
+    parser.add_argument('--qinclude', action='append', help='Include Paths for iquote', required=False)
+    parser.add_argument('--binclude', action='append', help='Include Paths for Block Include', required=False)
+    parser.add_argument('--cc1', help='<Required> cc1 Path', required=False)
+    parser.add_argument('--version', help='Get Version String of cc1', required=False)
+    parser.add_argument('--preproc', help='preproc path', required=False)
+    parser.add_argument('--charmap', help='preproc charmap', required=False)
+    parser.add_argument('-S', action='store_true', help='Ignore parameter as agbcc does not know it', required=False)
+    parser.add_argument('-o', help='Output Assembly file', required=False, dest='destination')
+    parser.add_argument('--no-parse', action='store_true', help='disable parsing of agbcc output (debug option)',
+                        required=False)
+    return parser.parse_known_args(argv)
 
-args, remainder = parser.parse_known_args()
-'''
-repopath = "/repos/tmc/"
-agbccpath = "/agbcc_build/tools/"
-agbcc = agbccpath+ "agbcc/bin/agbcc"
-preproc = repopath+ "tools/preproc/preproc"
-charmap = repopath+ "charmap.txt"
-'''
-if args.version:
-    git_proc = subprocess.run(['git', '--git-dir=' + args.version + '/.git', 'rev-parse', '--short', 'HEAD'],
-                              stdout=subprocess.PIPE)
-    print("pycc frontend for agbcc1 " + os.path.basename(args.version) + "@" + git_proc.stdout.decode('utf-8'))
-    exit(0)
 
-source = remainder[-1]
-cpp_args = ["cpp", "-nostdinc", "-undef"]
+def compile(args, source, remainder):
+    cpp_args = ["cpp", "-nostdinc", "-undef"]
 
-# Add Block Includes and Quote Includes
-if args.qinclude:
-    for q in args.qinclude:
-        cpp_args += ["-iquote", q]
+    # Add Block Includes and Quote Includes
+    if args.qinclude:
+        for q in args.qinclude:
+            cpp_args += ["-iquote", q]
 
-if args.binclude:
-    for b in args.binclude:
-        cpp_args += ["-I", b]
+    if args.binclude:
+        for b in args.binclude:
+            cpp_args += ["-I", b]
 
-thing = remainder[:-1]
-thing.remove('-g')
+    cpp_args += [source, "-o", source + ".i"]
+    subprocess.call(cpp_args)
+    if args.preproc and args.charmap:
+        pprocess = subprocess.Popen([args.preproc, source + '.i', args.charmap], stdout=subprocess.PIPE)
+        subprocess.call([args.cc1] + ['-o', args.destination + '.tmp'] + remainder, stdin=pprocess.stdout)
+    else:
+        with open(source + '.i', 'r') as a:
+            subprocess.call([args.cc1] + ['-o', args.destination + '.tmp'] + remainder, stdin=a)
 
-cpp_args += [source, "-o", source + ".i"]
-subprocess.call(cpp_args)
-if args.preproc and args.charmap:
-    pprocess = subprocess.Popen([args.preproc, source + '.i', args.charmap], stdout=subprocess.PIPE)
-    subprocess.call([args.cc1] + ['-o', args.destination + '.tmp'] + thing, stdin=pprocess.stdout)
-else:
-    with open(source + '.i', 'r') as a:
-        subprocess.call([args.cc1] + ['-o', args.destination + '.tmp'] + thing, stdin=a)
 
-tree, success = parse(args.destination + '.tmp')
-if not success:
-    raise ValueError('bad input file')
-ast = generate_ast(tree)
-apply_transformations(ast)
-with open(args.destination, 'w') as f_dst:
-    ASTDump(f_dst).visit(ast)
+def process_asm(args):
+    tree, success = parse(args.destination + '.tmp')
+    if not success:
+        raise ValueError('bad input file')
+    ast = generate_ast(tree)
+    apply_transformations(ast)
+    with open(args.destination, 'w') as destination_file:
+        ASTDump(destination_file).visit(ast)
 
-if os.path.exists(source + '.i'):
-    os.remove(source + '.i')
-'''
-source = sys.argv[-1]
-subprocess.call(["cpp", "-nostdinc", "-undef", "-iquote", repopath + "include" , "-I", agbccpath + "agbcc", "-I", agbccpath + "agbcc/include", source, "-o", source+".i"])
-pprocess = subprocess.Popen([preproc, source+".i", charmap], stdout=subprocess.PIPE)
-subprocess.call([agbcc] + sys.argv[1:-1], stdin=pprocess.stdout)
 
-if os.path.exists(source+".i"):
-        os.remove(source+".i")
-'''
+def cleanup(args, source):
+    for file in [f'{source}.i', f'{args.destination}.tmp']:
+        if os.path.exists(file):
+            os.remove(file)
+
+
+def main(argv):
+    args, remainder = parse_args(argv)
+    if args.version:
+        git_proc = subprocess.run(['git', '--git-dir=' + args.version + '/.git', 'rev-parse', '--short', 'HEAD'],
+                                  stdout=subprocess.PIPE)
+        print("pycc frontend for agbcc1 " + os.path.basename(args.version) + "@" + git_proc.stdout.decode('utf-8'))
+        exit(0)
+    source = remainder.pop(-1)
+    # disable debug information because the parser can't handle it yet
+    remainder.remove('-g')
+    try:
+        compile(args, source, remainder)
+
+        if not args.no_parse:
+            process_asm(args)
+        else:
+            copyfile(args.destination + '.tmp', args.destination)
+    finally:
+        cleanup(args, source)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
