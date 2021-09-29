c2b9579ac226858752b72fc693f3395ae0bbde7c
Henny022p <info@henny022.de>
2021-09-29 13:48:56 +0200

    cleaned pycat script

--- frontends/pycat.py
+++ frontends/pycat.py
@@ -1,23 +1,33 @@
 #!/usr/bin/env python3
 import argparse
+import sys
+
 from parser import parse, generate_ast, apply_transformations, ASTDump
 
-parser = argparse.ArgumentParser(description='Simplified "compiler" frontend, cats any input')
 
-parser.add_argument('--version', action='store_true', help='Get Version String of cc1', required=False, dest='version')
-parser.add_argument('-o', action='store', help='Output Assembly file', required=False, dest='destination')
-args, remainder = parser.parse_known_args()
+def parse_args(argv):
+    parser = argparse.ArgumentParser(description='Simplified "compiler" frontend, cats any input')
+    parser.add_argument('--version', action='store_true', help='Get Version String', required=False)
+    parser.add_argument('-o', action='store', help='Output Assembly file', required=False, dest='destination')
+    return parser.parse_known_args(argv)
 
-if args.version:
-    print("pycat frontend for cexplore")
-    quit()
 
-source = remainder[-1]
+def main(argv):
+    args, remainder = parse_args(argv)
+    if args.version:
+        print("pycat frontend for cexplore")
+        quit()
 
-tree, success = parse(source)
-if not success:
-    raise ValueError('bad input file')
-ast = generate_ast(tree)
-apply_transformations(ast)
-with open(args.destination, 'w') as f_dst:
-    ASTDump(f_dst).visit(ast)
+    source = remainder[-1]
+
+    tree, success = parse(source)
+    if not success:
+        raise ValueError('bad input file')
+    ast = generate_ast(tree)
+    apply_transformations(ast)
+    with open(args.destination, 'w') as f_dst:
+        ASTDump(f_dst).visit(ast)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
